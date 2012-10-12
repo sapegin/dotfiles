c0154f7dc3bb41aa0a07bcd176d28820ff92fc27
Artem Sapegin <artem@sapegin.ru>
2012-10-12 15:16:23 +0400

    Tweaks.

--- sync.py
+++ sync.py
@@ -9,7 +9,7 @@ Source: https://gist.github.com/490016
 import os
 import glob
 
-EXCLUDE = ['tools', 'color', 'sync.py', 'Readme.md']
+EXCLUDE = ['tools', 'color', 'setup',' sync.py', 'Readme.md']
 NO_DOT_PREFIX = ['bin']
 
 def force_remove(path):
