9baf769a4123c7a491a0b6f0e24052034dc38d82
Artem Sapegin <artem@sapegin.ru>
2012-10-19 15:16:12 +0400

    Some tweaks.

--- includes/bash_aliases.sh
+++ includes/bash_aliases.sh
@@ -28,6 +28,9 @@ export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40
 # Enable aliases to be sudo’ed
 alias sudo='sudo '
 
+# Avoid stupidity
+alias rm="rm -i"
+
 # Gzip-enabled `curl`
 #alias gurl="curl --compressed"
 
