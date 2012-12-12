3d02d949603a255c3e1d695026727369c8bf0878
Artem Sapegin <artem@sapegin.ru>
2012-12-12 15:18:25 +0400

    Bash: some cleaning.

--- tilde/bash_profile.bash
+++ tilde/bash_profile.bash
@@ -14,10 +14,10 @@ shopt -s cdspell
 # Save all lines of a multiple-line command in the same history entry (allows easy re-editing of multi-line commands)
 shopt -s cmdhist
 # Do not autocomplete when accidentally pressing Tab on an empty line. (It takes forever and yields "Display all 15 gazillion possibilites?")
-shopt -s no_empty_cmd_completion;
+shopt -s no_empty_cmd_completion
 
 # Do not overwrite files when redirecting using ">". Note that you can still override this with ">|"
-set -o noclobber;
+set -o noclobber
 
 # Enable some Bash 4 features when possible:
 # * `autocd`, e.g. `**/qux` will enter `./foo/bar/baz/qux`
