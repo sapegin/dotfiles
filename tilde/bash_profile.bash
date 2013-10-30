690edd82c5ad34d68e6ada25a6f393a5278e004a
Artem Sapegin <artem@sapegin.ru>
2013-10-30 23:33:36 +0400

    Revert Python path.

--- tilde/bash_profile.bash
+++ tilde/bash_profile.bash
@@ -43,6 +43,7 @@ PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
 [ -d /usr/local/bin ] && _prepend_path "/usr/local/bin"
 [ -d /usr/local/share/npm/bin ] && _prepend_path "/usr/local/share/npm/bin"
 [ -d /usr/local/opt/ruby/bin ] && _prepend_path "/usr/local/opt/ruby/bin"
+[ -d /usr/local/share/python ] && _prepend_path "/usr/local/share/python"
 command -v brew >/dev/null 2>&1 && _prepend_path "$(brew --prefix coreutils)/libexec/gnubin"
 [ -d ~/dotfiles/bin ] && _prepend_path "$HOME/dotfiles/bin"
 [ -d ~/bin ] && _prepend_path "$HOME/bin"
