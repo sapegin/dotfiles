4f10362d1e8c1fedc479170be1278045c29f9004
Artem Sapegin <artem@sapegin.ru>
2013-12-19 00:04:52 +0400

    Bash: Add Heroku bin to PATH.

--- tilde/bash_profile.bash
+++ tilde/bash_profile.bash
@@ -41,8 +41,9 @@ function _prepend_path() {
 # Construct $PATH
 PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
 [ -d /usr/local/bin ] && _prepend_path "/usr/local/bin"
-[ -d /usr/local/share/npm/bin ] && _prepend_path "/usr/local/share/npm/bin"
+[ -d /usr/local/heroku/bin ] && _prepend_path "/usr/local/heroku/bin"
 [ -d /usr/local/opt/ruby/bin ] && _prepend_path "/usr/local/opt/ruby/bin"
+[ -d /usr/local/share/npm/bin ] && _prepend_path "/usr/local/share/npm/bin"
 command -v brew >/dev/null 2>&1 && _prepend_path "$(brew --prefix coreutils)/libexec/gnubin"
 [ -d ~/dotfiles/bin ] && _prepend_path "$HOME/dotfiles/bin"
 [ -d ~/bin ] && _prepend_path "$HOME/bin"
