11cd50c9de5c19d2ccce009ac10069b748dbee3b
Artem Sapegin <artem@sapegin.ru>
2015-03-02 20:53:20 +0300

    New user script: Chrome new tab cleaner.

---
+++ userscripts/newtab.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name New tab
+// @description New tab cleaner
+// @include https://www.google.com/_/chrome/newtab*
+// @version 1.0
+// ==/UserScript==
+
+document.body.innerHTML = '';
