8a952fdaff82302b49716a9291fc117a9121caeb
Artem Sapegin <artem@sapegin.ru>
2015-03-02 23:06:01 +0300

    New tab: should work for any language.

--- userscripts/newtab.user.js
+++ userscripts/newtab.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name New tab
 // @description New tab cleaner
-// @include https://www.google.com/_/chrome/newtab*
-// @version 1.0
+// @include https://www.google.*/_/chrome/newtab*
+// @version 1.0.1
 // ==/UserScript==
 
 document.body.innerHTML = '';
