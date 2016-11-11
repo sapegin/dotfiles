f542c19459326fa2982b35521b91037ea86a3ce3
Artem Sapegin <artem@sapegin.ru>
2016-11-11 10:37:00 +0100

    Slate: add left half and right half shortcuts.

--- tilde/slate.js
+++ tilde/slate.js
@@ -12,7 +12,23 @@ var middle = S.op('move', {
 	height: 'screenSizeY'
 });
 
+var leftHalf = S.op('move', {
+	x: 'screenOriginX',
+	y: 'screenOriginY',
+	width: 'screenSizeX/2',
+	height: 'screenSizeY'
+});
+
+var rightHalf = S.op('move', {
+	x: 'screenSizeX/2',
+	y: 'screenOriginY',
+	width: 'screenSizeX/2',
+	height: 'screenSizeY'
+});
+
 S.bnda({
 	'f:ctrl;alt': fullscreen,
-	'm:ctrl;alt': middle
+	'm:ctrl;alt': middle,
+	'left:cmd;ctrl;alt': leftHalf,
+	'right:cmd;ctrl;alt': rightHalf,
 });
