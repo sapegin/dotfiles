f9774a8e76cf99a6320e2ba32492ec575d5e6902
Artem Sapegin <artem@sapegin.ru>
2017-09-06 19:58:43 +0200

    Mrm: Add jsconfig task

---
+++ mrm/jsconfig/index.js
@@ -0,0 +1,18 @@
+// @ts-check
+'use strict';
+
+const { json } = require('mrm-core');
+
+function task() {
+	json('jsconfig.json')
+		.merge({
+			compilerOptions: {
+				lib: ['esnext'],
+			},
+			exclude: ['node_modules'],
+		})
+		.save();
+}
+task.description = 'Adds jsconfig.json';
+
+module.exports = task;
