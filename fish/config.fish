2bb64b5d7ddd3008b1398b2b93f3042672f72366
Artem Sapegin <artem@sapegin.ru>
2018-09-03 07:31:48 +0200

    Fish: Disable path shortening in prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 # Remove "Welcome to fish" message
 set fish_greeting
 
+# Disable path shortening in prompt
+set fish_prompt_pwd_dir_length 0
+
 source ~/.config/fish/env.fish
 source ~/.config/fish/path.fish
 source ~/.config/fish/aliases.fish
