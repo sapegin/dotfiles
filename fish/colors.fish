bdff0ccbb0e5769e7de2505b1bf97dace2ae5f2a
Artem Sapegin <asapegin@wayfair.com>
2018-09-03 11:21:46 +0200

    Fish: Use different black color

--- fish/colors.fish
+++ fish/colors.fish
@@ -1,9 +1,9 @@
 # Fish colors
 # Make autocompletions "invisible" (same color as for the current line)
 # set fish_color_autosuggestion 362c24 362c24
-set fish_color_autosuggestion brblack --dim
+set fish_color_autosuggestion black --dim
 set fish_color_command cyan --bold
-set fish_color_comment brblack --dim
+set fish_color_comment black --dim
 set fish_color_cwd white
 set fish_color_cwd_root red
 set fish_color_error red
@@ -21,6 +21,6 @@ set fish_color_status ff0
 set fish_color_user ff0
 set fish_color_valid_path --underline
 set fish_pager_color_completion white
-set fish_pager_color_description brblack --dim
+set fish_pager_color_description black --dim
 set fish_pager_color_prefix normal
 set fish_pager_color_progress cyan
