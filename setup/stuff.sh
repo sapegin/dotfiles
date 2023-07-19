b52bec2c66983dd3963b14179311fc7165898d92
Artem Sapegin <artem@sapegin.ru>
2023-07-19 09:18:51 +0200

    Reorganize Homebrew setup

--- setup/stuff.sh
+++ setup/stuff.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# Installs Homebrew, Git, git-friendly, Node.js, etc.
+# Installs Git, git-friendly, Node.js, and many other command line tools
+
+# Check that Homebrew is installed
+command -v brew >/dev/null 2>&1 || { error "Homebrew not installed: https://brew.sh/"; exit 1; }
 
 # Ask for the administrator password upfront
 sudo -v
 
-# Install Homebrew
-#command -v brew >/dev/null 2>&1 || ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+# Install XCode command line tools, and accept its license
+xcode-select --install
+xcodebuild -license
 
 # Update Homebrew and already installed packages
 brew update
-
-# GNU core utilities (those that come with macOS are outdated)
-brew install coreutils
-
-# GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-brew install tree
 
 # Git
 brew install git
@@ -27,6 +24,15 @@ brew install git-friendly/git-friendly/git-friendly
 # Extend global $PATH
 echo -e "setenv PATH $HOME/dotfiles/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin" | sudo tee /etc/launchd.conf
 
+# Node
+# n, Node version manager
+brew install n
+npm config set loglevel warn
+
+# Npm packages
+npm install -g npm-upgrade
+npm install -g tldr
+
 # Everything else
 brew install bat
 brew install fd
@@ -36,18 +42,10 @@ brew install micro
 brew install proselint
 brew install ripgrep
 brew install starship
-brew install tldr
 brew install git-delta
 brew install bat-extras
+brew install tree
 brew install zsh-syntax-highlighting
-
-# Node
-# n, Node version manager
-brew install n
-npm config set loglevel warn
-
-# Npm
-npm i -g npm-upgrade
 
 # Remove outdated versions from the cellar
 brew cleanup
