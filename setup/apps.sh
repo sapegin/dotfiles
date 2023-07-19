f3f14545e6cf1c0680b9c53570a1ee085f1dd8eb
Artem Sapegin <artem@sapegin.ru>
2023-07-19 09:18:29 +0200

    Add Cask

---
+++ setup/apps.sh
@@ -0,0 +1,33 @@
+# Installs GUI apps
+# https://formulae.brew.sh/cask/
+
+# Check that Homebrew is installed
+command -v brew >/dev/null 2>&1 || { error "Homebrew not installed: https://brew.sh/"; exit 1; }
+
+brew install --cask 1password
+brew install --cask adobe-creative-cloud
+brew install --cask alfred
+brew install --cask bartender
+brew install --cask colorsnapper
+brew install --cask dash
+brew install --cask dropbox
+brew install --cask firefox
+brew install --cask fluor
+brew install --cask forklift
+brew install --cask github # GitHub Desktop
+brew install --cask kap
+brew install --cask kindle
+brew install --cask messenger
+brew install --cask microsoft-edge
+brew install --cask mimestream
+brew install --cask notion
+brew install --cask numi
+brew install --cask optimage
+brew install --cask quitter
+brew install --cask rectangle
+brew install --cask shottr
+brew install --cask slack
+brew install --cask telegram
+brew install --cask visual-studio-code
+brew install --cask whatsapp
+brew install --cask zoom
