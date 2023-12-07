b64638aa9e47409b755e638a7cf6590b61f339cb
Artem Sapegin <artem@sapegin.ru>
2023-12-07 14:08:04 +0100

    Zsh: fix Homebrew path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -8,12 +8,12 @@ function _prepend_path() {
 # Construct $PATH
 # 1. Default paths
 # 2. ./node_modules/.bin - shorcut to run locally installed Node bins
-# 3. Custom bin folder for n, Ruby, CoreUtils, dotfiles, etc.
+# 3. Custom bin folder for n, Ruby, Homebrew, dotfiles, etc.
 PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:./node_modules/.bin'
 [ -d "$N_PREFIX/bin" ] && _prepend_path "$N_PREFIX/bin"
 [ -d /usr/local/bin ] && _prepend_path "/usr/local/bin"
 [ -d /usr/local/opt/ruby/bin ] && _prepend_path "/usr/local/opt/ruby/bin"
-[ -d /usr/local/opt/coreutils/libexec/gnubin ] && _prepend_path "/usr/local/opt/coreutils/libexec/gnubin"
+[ -d /opt/homebrew/bin ] && _prepend_path "/opt/homebrew/bin"
 [ -d ~/dotfiles/bin ] && _prepend_path "$HOME/dotfiles/bin"
 [ -d ~/bin ] && _prepend_path "$HOME/bin"
 export PATH
