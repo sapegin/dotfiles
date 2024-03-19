703a966fdf4e560a5c3160d335b4a6272455cc10
Artem Sapegin <artem@sapegin.ru>
2024-03-19 16:20:51 +0100

    Less: remove highlighting options
    They weren't working but appearing as is in the terminal. Highlighting seems to fork fine without them.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -12,7 +12,7 @@ else
 	export EDITOR='micro'
 fi
 
-# Make less the default pager, add some options and enable syntax highlight using source-highlight
+# Make less the default pager, add some options
 [ -n "$LESSPIPE" ] && export LESSOPEN="| ${LESSPIPE} %s"
 less_options=(
 	# If the entire text fits on one screen, just show it and quit. (Be more
@@ -39,15 +39,6 @@ less_options=(
 );
 export LESS="${less_options[*]}"
 export PAGER='less'
-
-# Highlighting inside manpages and elsewhere
-export LESS_TERMCAP_mb="\e'[01;31m'"       # begin blinking
-export LESS_TERMCAP_md="\e'[01;38;5;74m'"  # begin bold
-export LESS_TERMCAP_me="\e'[0m'"           # end mode
-export LESS_TERMCAP_se="\e'[0m'"           # end standout-mode
-export LESS_TERMCAP_so="\e'[38;5;246m'"    # begin standout-mode - info box
-export LESS_TERMCAP_ue="\e'[0m'"           # end underline
-export LESS_TERMCAP_us="\e'[04;38;5;146m'" # begin underline
 
 # Bat: https://github.com/sharkdp/bat
 export BAT_THEME="Squirrelsong Dark"
