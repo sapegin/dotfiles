48c7f519c17a17323cc23d8dfc594f97625f8c7a
Artem Sapegin <artem@sapegin.ru>
2024-03-21 08:29:53 +0100

    Icons: New alternative app icons

--- setup/icons.sh
+++ setup/icons.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 # Better app icons
+# A better alternative to this script: https://replacicon.app/
 # https://www.sethvargo.com/replace-icons-osx/
 
 sudo -v
 
 ICONS_DIR="$HOME/dotfiles/icons"
 
-# ForkLift
-cp "$ICONS_DIR/ForkLift.icns" "/Applications/ForkLift.app/Contents/Resources/AppIcon.icns"
-touch "/Applications/ForkLift.app"
+# Nimble Commander
+# https://www.iconarchive.com/show/variations-2-icons-by-guillendesign/Floppy-icon.html
+cp "$ICONS_DIR/Floppy.icns" "/Applications/Nimble Commander.app/Contents/Resources/Icon.icns"
+touch "/Applications/Nimble Commander.app"
 
+# WezTerm
+# https://dribbble.com/shots/656627-Terminal-Macintosh-Icon
+cp "$ICONS_DIR/Terminal.icns" "/Applications/WezTerm.app/Contents/Resources/terminal.icns"
+touch "/Applications/WezTerm.app"
+
+# Visual Studio Code
+# https://dribbble.com/shots/15424559-VS-Code-replacement-icon
+cp "$ICONS_DIR/Code.icns" "/Applications/Visual Studio Code.app/Contents/Resources/Code.icns"
+touch "/Applications/Visual Studio Code.app"
+
+# TODO: Doesn't work
 # Edge
 cp "$ICONS_DIR/InternetExplorer.icns" "/Applications/Microsoft Edge.app/Contents/Resources/app.icns"
 touch "/Applications/Microsoft Edge.app"
