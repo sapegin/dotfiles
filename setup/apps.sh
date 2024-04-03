57904c550489f535287b5b0e4e35b271b5ef0231
Artem Sapegin <artem@sapegin.ru>
2024-04-03 10:23:47 +0200

    Comment some apps
    I probably should split it somehow, or print additional apps instead of installing them.

--- setup/apps.sh
+++ setup/apps.sh
@@ -15,19 +15,19 @@ command -v brew > /dev/null 2>&1 || {
 
 # Apps
 brew install --cask 1password
-brew install --cask adobe-creative-cloud
+# brew install --cask adobe-creative-cloud
 brew install --cask alfred
 brew install --cask bartender
 brew install --cask colorsnapper
 brew install --cask coteditor
 brew install --cask dash
 brew install --cask devtoys
-brew install --cask dropbox
+# brew install --cask dropbox
 brew install --cask firefox
 brew install --cask github # GitHub Desktop
 brew install --cask kap
 brew install --cask kindle
-brew install --cask messenger
+# brew install --cask messenger
 brew install --cask microsoft-edge
 brew install --cask mimestream
 brew install --cask nimble-commander
@@ -37,12 +37,12 @@ brew install --cask optimage
 brew install --cask quitter
 brew install --cask rectangle
 brew install --cask shottr
-brew install --cask slack
+# brew install --cask slack
 brew install --cask telegram
 brew install --cask visual-studio-code
 brew install --cask wezterm
 brew install --cask whatsapp
-brew install --cask zoom
+# brew install --cask zoom
 
 # Fonts
 brew tap homebrew/cask-fonts
