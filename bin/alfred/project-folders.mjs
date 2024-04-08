0de9461d87907f1758f01f8c4bf181e6958765f1
Artem Sapegin <artem@sapegin.ru>
2024-04-08 10:28:19 +0200

    Alfred project opener: Add customizable work projects location

--- bin/alfred/project-folders.mjs
+++ bin/alfred/project-folders.mjs
@@ -11,9 +11,24 @@
 import fs from 'fs';
 import path from 'path';
 import userHome from 'user-home';
+import untildify from 'untildify';
+import dotenv from 'dotenv';
 import glob from 'glob';
 
-const folders = [glob.sync(`${userHome}/_/*/`), `${userHome}/dotfiles/`].flat();
+dotenv.config({ path: untildify('~/.env') });
+
+const folders = [
+	// Personal projects
+	glob.sync(untildify(`~/_/*/`)),
+
+	// Work projects
+	process.env.WORK_PROJECTS_DIR
+		? glob.sync(untildify(`${process.env.WORK_PROJECTS_DIR}/*/`))
+		: [],
+
+	// Extra projects
+	untildify(`~/dotfiles/`),
+].flat();
 
 const items = folders
 	.map((file) => {
