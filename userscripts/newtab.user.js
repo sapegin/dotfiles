814c9baf478dbda5a35b62e593e862485d8da6ef
Artem Sapegin <artem@sapegin.ru>
2024-09-13 09:11:11 +0200

    Remove unused new tab user script

--- userscripts/newtab.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name New tab
-// @description New tab cleaner
-// @include https://www.google.*/_/chrome/newtab*
-// @version 1.0.1
-// ==/UserScript==
-
-document.body.innerHTML = '';
