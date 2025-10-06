c2bbf0916fcb21812ffeb3e4389a1804692caa40
Artem Sapegin <artem@sapegin.me>
2025-10-06 15:37:39 +0200

    Tweak ls/eza colors to only use base palette

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -50,21 +50,21 @@ LS_COLORS+="or=31:"    # Broken symlinks
 export LS_COLORS
 
 # Eza colors: https://github.com/eza-community/eza/blob/main/man/eza_colors.5.md
-EZA_COLORS="reset:$LS_COLORS"                      # Reset default colors, like making everything yellow
-EZA_COLORS+="da=36:"                               # Timestamps
-EZA_COLORS+="ur=0:uw=0:ux=0:ue=0:"                 # User permissions
-EZA_COLORS+="gr=0:gw=0:gx=0:"                      # Group permissions
-EZA_COLORS+="tr=0:tw=0:tx=0:"                      # Other permissions
-EZA_COLORS+="xa=0:"                                # Extended attribute marker ('@')
-EZA_COLORS+="xx=38;5;240:"                         # Punctuation ('-')
-EZA_COLORS+="nb=38;5;240:"                         # Files under 1 KB
-EZA_COLORS+="nk=0:"                                # Files under 1 MB
-EZA_COLORS+="nm=37:"                               # Files under 1 GB
-EZA_COLORS+="ng=38;5;250:"                         # Files under 1 TB
-EZA_COLORS+="nt=38;5;255:"                         # Files over 1 TB
-EZA_COLORS+="do=32:*.md=32:"                       # Documents
-EZA_COLORS+="co=35:*.zip=35:"                      # Archives
-EZA_COLORS+="tm=38;5;242:cm=38;5;242:.*=38;5;242:" # Hidden and temporary files
+EZA_COLORS="reset:$LS_COLORS"       # Reset default colors, like making everything yellow
+EZA_COLORS+="da=36:"                # Timestamps
+EZA_COLORS+="ur=0:uw=0:ux=0:ue=0:"  # User permissions
+EZA_COLORS+="gr=0:gw=0:gx=0:"       # Group permissions
+EZA_COLORS+="tr=0:tw=0:tx=0:"       # Other permissions
+EZA_COLORS+="xa=0:"                 # Extended attribute marker ('@')
+EZA_COLORS+="xx=90:"                # Punctuation ('-') (black)
+EZA_COLORS+="nb=90:"                # Files under 1 KB (black)
+EZA_COLORS+="nk=90:"                # Files under 1 MB (black)
+EZA_COLORS+="nm=37:"                # Files under 1 GB (white)
+EZA_COLORS+="ng=97:"                # Files under 1 TB (bright white)
+EZA_COLORS+="nt=97:"                # Files over 1 TB (bright white)
+EZA_COLORS+="do=32:*.md=32:"        # Documents (green)
+EZA_COLORS+="co=35:*.zip=35:"       # Archives (magenta)
+EZA_COLORS+="tm=90:cm=90:.*=90:"    # Hidden and temporary files (black)
 export EZA_COLORS
 
 # Homebrew install badge: beer sucks, coffee rules
