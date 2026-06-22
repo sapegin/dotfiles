306c2ce4e8b6390ddf38147b98155dc5ca590606
Artem Sapegin <artem@sapegin.me>
2026-06-22 17:05:35 +0200

    Textlint: Enable missing misspellings rule

--- textlint/preset/textlint-preset.cjs
+++ textlint/preset/textlint-preset.cjs
@@ -2,17 +2,18 @@ module.exports = {
   rules: {
     apostrophe: require('../../../_/raccoon-textlint/rules/apostrophe').default,
     diacritics: require('../../../_/raccoon-textlint/rules/diacritics').default,
+    misspellings: require('../../../_/raccoon-textlint/rules/misspellings')
+      .default,
     quotes: require('../../../_/raccoon-textlint/rules/quotes').default,
     'stop-words': require('../../../_/raccoon-textlint/rules/stop-words')
       .default,
     terminology: require('../../../_/raccoon-textlint/rules/terminology')
       .default,
-    misspellings: require('../../../_/raccoon-textlint/rules/misspellings')
-      .default,
   },
   rulesConfig: {
     apostrophe: true,
     diacritics: true,
+    misspellings: true,
     quotes: true,
     'stop-words': {
       severity: 'warning',
