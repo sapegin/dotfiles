0f520f9396445c6c5a273ff5f9b422261d4c0ac7
Artem Sapegin <artem@sapegin.me>
2026-06-30 17:13:16 +0200

    lint-text: Make it work with relative paths

--- src/bin/lint-text.ts
+++ src/bin/lint-text.ts
@@ -12,12 +12,19 @@
 import { spawnSync } from 'node:child_process';
 import path from 'node:path';
 import { dirs } from '../util/consts.ts';
+import { untildify } from '../util/tildify.ts';
 
 const result = spawnSync(
   path.join(dirs.dotfiles, 'node_modules/.bin/textlint'),
-  ['--config', 'textlint/textlint.config.cjs', ...process.argv.slice(2)],
+  [
+    '--config',
+    path.join(dirs.dotfiles, 'textlint/textlint.config.cjs'),
+    ...process.argv.slice(2).map(untildify),
+  ],
   {
-    cwd: dirs.dotfiles,
+    // Run in the caller’s directory so relative file globs are resolved against
+    // it instead of the dotfiles repository.
+    cwd: process.cwd(),
     stdio: 'inherit',
   }
 );
